04f7c7336c451706895cb188afddd50e3e4f1fc1
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-01-30 19:31:39 +0530

    Setup Express App

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
-console.log('Hello Typescript');
-function Hello() {
-    console.log('test');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+import serverConfig from './config/server.config.js';
+function startServer() {
+    const app = express();
+
+    //middlewares
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(cors());
+
+    app.get('/test', (req, res) => {
+        res.status(200).send('Testing API');
+    });
+
+    app.listen(serverConfig.PORT, () => {
+        console.log(`Server is running on port : ${serverConfig.PORT}`);
+    });
 }
-Hello();
+startServer();
